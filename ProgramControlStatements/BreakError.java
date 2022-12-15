63312d9082de15ff743b07a30a985dd4a84fdbc7
Wisely Kwek <wisely.kwek.2020@scis.smu.edu.sg>
2022-12-15 12:12:42 +0800

    added comments

--- ProgramControlStatements/BreakError.java
+++ ProgramControlStatements/BreakError.java
@@ -7,7 +7,7 @@ public class BreakError {
             System.out.print("Pass " + i + ": ");
         }
         for (int j=0; j<100; j++){
-            if (j == 10) break one; // WRONG
+//            if (j == 10) break one; // WRONG
             System.out.print(j + " ");
         }
     }
